8271d53c64e0fcb309f2c1ba9d79f6a3d8fd231d
Ellen Poe <ellen@ellenhp.me>
2025-06-23 16:02:19 -0700

    fix: integration tests now tolerate limited discrepencies in leg endpoints

--- solari/src/api/mod.rs
+++ solari/src/api/mod.rs
@@ -60,7 +60,7 @@ pub enum SolariLeg {
     },
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct SolariItinerary {
     pub start_location: LatLng,
     pub end_location: LatLng,
@@ -75,4 +75,83 @@ pub struct SolariItinerary {
     )]
     pub end_time: OffsetDateTime,
     pub legs: Vec<SolariLeg>,
+}
+
+impl PartialEq for SolariItinerary {
+    fn eq(&self, other: &Self) -> bool {
+        let meta_eq = self.start_location == other.start_location
+            && self.end_location == other.end_location
+            && self.start_time == other.start_time
+            && self.end_time == other.end_time
+            && self.legs.len() == other.legs.len();
+
+        if !meta_eq {
+            return false;
+        }
+
+        // The core problem we're trying to solve here is that it doesn't matter for the purposes of integration testing whether I get onto a bus at stop 1 or stop 2, as long as it's the same bus.
+        for (leg1, leg2) in self.legs.iter().zip(other.legs.iter()) {
+            let legs_eq = match (leg1, leg2) {
+                (
+                    SolariLeg::Transit {
+                        start_time,
+                        end_time,
+                        start_location,
+                        end_location,
+                        route_shape,
+                        transit_route,
+                        transit_agency,
+                    },
+                    SolariLeg::Transit {
+                        start_time: other_start_time,
+                        end_time: other_end_time,
+                        start_location: other_start_location,
+                        end_location: other_end_location,
+                        route_shape: other_route_shape,
+                        transit_route: other_transit_route,
+                        transit_agency: other_transit_agency,
+                    },
+                ) => {
+                    transit_route == other_transit_route
+                        && transit_agency == other_transit_agency
+                        && (start_location != other_start_location
+                            || start_time == other_start_time)
+                        && (end_location != other_end_location || end_time == other_end_time)
+                        && (start_location != other_start_location
+                            || end_location != other_end_location
+                            || route_shape == other_route_shape)
+                        && (start_location == other_start_location
+                            || end_location == other_end_location)
+                }
+                (SolariLeg::Transit { .. }, SolariLeg::Transfer { .. }) => false,
+                (SolariLeg::Transfer { .. }, SolariLeg::Transit { .. }) => false,
+                (
+                    SolariLeg::Transfer {
+                        start_time,
+                        end_time,
+                        start_location,
+                        end_location,
+                        route_shape,
+                    },
+                    SolariLeg::Transfer {
+                        start_time: other_start_time,
+                        end_time: other_end_time,
+                        start_location: other_start_location,
+                        end_location: other_end_location,
+                        route_shape: other_route_shape,
+                    },
+                ) => {
+                    (start_location == other_start_location || end_location == other_end_location)
+                        && (start_time == other_start_time || end_time == other_end_time)
+                        && (start_location != other_start_location
+                            || end_location != other_end_location
+                            || route_shape == other_route_shape)
+                }
+            };
+            if !legs_eq {
+                return false;
+            }
+        }
+        false
+    }
 }
